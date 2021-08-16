422b77f46f58e2d1a46ec3aca47090803737414e
Maciek Ułanowicz <maciekbsz@gmail.com>
2021-08-16 12:29:03 +0200

    added background photo to aboutme component

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 import styled from 'styled-components'
 import { Paper, Typography } from '@material-ui/core'
 import img from '../img/AboutMePageImg.jpg'
+import bgImg from '../img/AboutMePageBG.jpg'
 
 const Box = styled.div`
 	position: relative;
-	top: 3vw;
-	width: 100%;
+	top: 15px;
+	height: 90vh;
 	display: flex;
 	flex-direction: column;
+	background-image: url(${bgImg});
+	background-size: cover;
+	background-repeat: no-repeat;
 	align-items: center;
 	justify-content: center;
 `
 const Photo = styled.div`
 	width: 500px;
-	height: 500px;
-	border-radius: 50%;
 	background-image: url(${img});
+	height: 500px;
 	background-size: cover;
+	border-radius: 50%;
 	background-repeat: no-repeat;
 	background-position: 55%;
 `
@@ -27,12 +31,12 @@ const Bio = styled.div`
 	align-items: center;
 	justify-content: space-evenly;
 	text-align: center;
-	width: 400px;
-	height: 200px;  
+	width: 500px;
+	height: 200px;
 	margin: 30px;
 	padding: 20px;
 `
-const AboutMe = ({MyInfo}) => {
+const AboutMe = ({ MyInfo }) => {
 	return (
 		<Box>
 			<Photo></Photo>
